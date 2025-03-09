07e945b1245cca15756cd26d79eee8c66305571f
nicunursekatie <katielong2316@gmail.com>
2025-03-09 17:05:13 -0400

    Update improvedscript.js

--- improvedscript.js
+++ improvedscript.js
@@ -212,7 +212,7 @@ document.addEventListener('DOMContentLoaded', function() {
   // Populate feedingTime select with example time options if not already
   if (feedingTimeSelect) {
     feedingTimeSelect.innerHTML = "";
-    const times = ["00:00", "06:00", "12:00", "18:00"];
+    const times = ["02:00", "02:30", "03:00", "05:00, "05:30", "06:00", "08:00", "08:30", "09:00", "11:00", "11:30", "12:00", "14:00", "14:30", "15:00", "17:00", "17:30", "18:00", "20:00", "20:30", "21:00", "23:00", "23:30", "00:00"];
     times.forEach(time => {
       const option = document.createElement("option");
       option.value = time;
